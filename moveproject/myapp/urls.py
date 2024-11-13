62e58e51eb0b8645f00eab38a711a1ad552da31e
Vishag S <vishaghs03@gmail.com>
2024-11-13 19:01:47 +0530

    Initial commit

--- moveproject/myapp/urls.py
+++ moveproject/myapp/urls.py
@@ -1,10 +1,13 @@
 # events/urls.py
 from django.urls import path
-from .views import main_view, event_creation_view, login_view, add_finance_view
+from . import views
 
 urlpatterns = [
-    path('index/', main_view, name='index'),
-    path('create-event/', event_creation_view, name='create_event'),
-    path('add-finance/<int:event_id>/', add_finance_view, name='add_finance'),  # New path for adding finance
-    path('', login_view, name='login'),
+    path('index/', views.main_view, name='index'),
+    path('create-event/', views.event_creation_view, name='create_event'),
+    path('add-finance/<int:event_id>/', views.add_finance_view, name='add_finance'),  # New path for adding finance
+    path('delete-event/<int:event_id>/', views.delete_event, name='delete_event'),
+    
+    path('', views.login_view, name='login'),
+    path('edit-event/<int:event_id>/', views.edit_event, name='edit_event'),
 ]
